516e0c5913d5d4aea0ba74fe35369fd00da12f82
Natalia <157173044+natalatatis@users.noreply.github.com>
2025-09-26 11:22:29 +0000

    Cambio de colores de íconos

--- my-app/src/theme.js
+++ my-app/src/theme.js
@@ -1,38 +1,55 @@
 // src/theme.js
-// Paleta de colores a utilizar
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-    typography: {
+  typography: {
     fontFamily: "'Manrope', sans-serif",
   },
-  
+
   palette: {
     primary: {
-      main: '#49769f', // dark navy
+      main: "#49769f", // dark navy
     },
     secondary: {
-      main: '#0a4174', // deep blue
+      main: "#0a4174", // deep blue
     },
     info: {
-      main: '#001d39', // medium blue 
+      main: "#001d39", // medium blue
     },
     success: {
-      main: '#4e8ea2', // teal
+      main: "#4e8ea2", // teal
     },
     warning: {
-      main: '#6ea2b3', // aqua
+      main: "#6ea2b3", // aqua
     },
     customLight: {
-      main: '#7bbde8', // light sky blue
+      main: "#7bbde8", // light sky blue
     },
     customExtraLight: {
-      main: '#bdd8e9', // pale blue
+      main: "#bdd8e9", // pale blue
+    },
+
+    accentOrange: {
+      main: "#ff914d", // warm orange
+    },
+    accentGreen: {
+      main: "#6cc551", // fresh green
+    },
+    accentPurple: {
+      main: "#9b5de5", // vivid purple
+    },
+    accentPink: {
+      main: "#f15bb5", // bright pink
+    },
+    accentRed: {
+      main: "#ff0000",
     },
   },
+
   shape: {
     borderRadius: 12,
   },
+
   typography: {
     fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
   },
